6473ddc6dc10692a198ec20dbab281e2803913ec
박찬우 <pcw1443@gmail.com>
2026-03-21 13:35:59 +0900

    Step 7: 데이터 불러오기 완료

--- week02/pandas numpy.py
+++ week02/pandas numpy.py
@@ -105,3 +105,18 @@ print(iris.groupby('class').std())
 print(iris.drop('class', axis = 1).agg(['sum','mean','std']))
 
 # print(iris.agg(['sum','mean','std']))  #class 컬럼으로 인해 에러 발생
+
+print(iris['class'].unique())
+print(iris['class'].nunique())
+print(iris['class'].value_counts())
+
+# 예제 데이터 생성
+data =  {
+     'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+    'age': [25, 30, 35, 28, 40],
+    'salary': [70000.00, 80000.00, 90000.00, 60000.00, 95000.00]
+}
+
+# Dataframe 생성
+df = pd.DataFrame(data)
+print(df.head())
