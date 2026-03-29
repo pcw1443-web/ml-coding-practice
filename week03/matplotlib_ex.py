d8b6518752051fb07b3238a4b4779f6f3cb9e441
박찬우 <pcw1443@gmail.com>
2026-03-29 13:20:34 +0900

    Step 2: 데이터 및 시각화 코드 업데이트 완료

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -6,3 +6,15 @@ import matplotlib.pyplot as plt
 # 타이타닉 CSV 파일 불러오기
 # (참고: 실행 환경에 '3.1.1.titanic.csv' 파일이 있어야 합니다.)
 titanic = pd.read_csv('3.1.1.titanic.csv')
+
+# 데이터 처음 5개의 행 출력
+print(titanic.head())
+
+# 열에 대한 요약 정보 확인
+print(titanic.info())
+
+"""### **선 그래프 : 객실 등급에 따른 생존율 표시하기**"""
+
+# 객실 등급에 따른 생존자와 사망자의 평균 계산 
+pclass_survived_mean = titanic.groupby('Pclass')['Survived'].mean().reset_index()
+
