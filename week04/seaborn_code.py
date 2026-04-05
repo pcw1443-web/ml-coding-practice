06bb106af6612a7bed7f9e18ab53dcbd7ff74d8d
박찬우 <pcw1443@gmail.com>
2026-04-05 15:15:47 +0900

    Step 1: 코드 생성

---
+++ week04/seaborn_code.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# 시본 라이브러리 불러오기
+import seaborn as sns
+
+# **팁(tips) 데이터셋 불러오기**
+tips = sns.load_dataset('tips')
+print(tips.head())
+
+tips.info()
+
+# **범주형 변수 산점도 그래프**
+
+import matplotlib.pyplot as plt
