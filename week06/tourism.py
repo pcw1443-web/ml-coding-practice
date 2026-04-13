0d3886f1fa9864fd6b9de24645ddea9265670b16
박찬우 <pcw1443@gmail.com>
2026-04-13 16:13:54 +0900

    Step 3: 코드 생성

--- week06/tourism.py
+++ week06/tourism.py
@@ -24,3 +24,37 @@ def main():
     result_df = pd.DataFrame(result, columns = columns)
     # 한글 깨짐 방지를 위해 cp949 인코딩 사용
     result_df.to_csv('./%s_%s_%d_%s.csv' % (natName, ed_cd, nStartYear, dataEND), index = False, encoding = 'cp949')
+
+def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
+    jsonResult = []
+    result = []
+    natName = ''
+    ed = ''
+
+    for year in range(nStartYear, nEndYear+1):
+        for month in range(1, 13):
+            yyyymm = "{0}{1:0>2}".format(str(year), str(month))
+            jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)
+            
+            if (jsonData['response']['header']['resultMsg'] == 'OK'):
+                # 더 이상 데이터가 없는 경우 종료
+                if jsonData['response']['body']['items'] == '':
+                    dataEND = "{0}{1:0>2}".format(str(year), str(month-1))
+                    print("데이터 없음.... \n제공되는 통계 데이터는 %s년 %s월까지입니다." % (str(year), str(month-1)))
+                    break
+                
+                print(json.dumps(jsonData, indent = 4, sort_keys = True, ensure_ascii = False))
+
+                natName = jsonData['response']['body']['items']['item']['natKorNm']
+                natName = natName.replace(' ', '')
+                num = jsonData['response']['body']['items']['item']['num']
+                ed = jsonData['response']['body']['items']['item']['ed']
+                
+                print('[ %s_%s : %s ]' % (natName, yyyymm, num))
+                print('------------------------------------------------------')
+                
+                jsonResult.append({'nat_name': natName, 'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num})
+                result.append([natName, nat_cd, yyyymm, num])
+
+    return (jsonResult, result, natName, ed)
+
