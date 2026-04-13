1a4c5c3553df3650b15588aa27ae2c686f62e0df
박찬우 <pcw1443@gmail.com>
2026-04-13 16:23:01 +0900

    Step 5: 코드 생성

---
+++ week06/naverapi.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import urllib.request
+import datetime
+import json
+
+# [중요] 네이버 개발자 센터에서 발급받은 ID와 Secret을 입력해야 합니다.
+client_id = 'Client ID'
+client_secret = 'Client Secret'
