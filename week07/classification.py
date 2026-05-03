7fc3b964f832162343ecafcb669dfd08e4736b3b
박찬우 <pcw1443@gmail.com>
2026-05-03 14:14:18 +0900

    Step 4: 코드 작성

--- week07/classification.py
+++ week07/classification.py
@@ -45,3 +45,29 @@ sgd_clf = SGDClassifier(random_state=42)
 sgd_clf.fit(X_train, y_train_5)
 
 sgd_clf.predict([some_digit])
+
+# 성능 측정 - 교차 검증을 사용한 정확도 측정
+from sklearn.model_selection import cross_val_score
+
+cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+from sklearn.dummy import DummyClassifier
+
+dummy_clf = DummyClassifier()
+dummy_clf.fit(X_train, y_train_5)
+print(any(dummy_clf.predict(X_train)))
+
+print(cross_val_score(dummy_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
+
+#오차 행렬 
+from sklearn.model_selection import cross_val_predict
+
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv= 3)
+
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_train_5, y_train_pred)
+print(cm)
+
+y_train_perfect_predictions = y_train_5  # 완벽한 분류기일 경우
+print(confusion_matrix(y_train_5, y_train_perfect_predictions))
