2022273fb4c840dddabf6480a191a19f47fcd2f8
박찬우 <pcw1443@gmail.com>
2026-05-03 14:02:43 +0900

    Step 2: 코드 작성

--- week07/classification.py
+++ week07/classification.py
@@ -4,3 +4,22 @@ from sklearn.datasets import fetch_openml
 mnist = fetch_openml('mnist_784', as_frame=False)
 
 print(mnist.keys())  # data와 target만 사용
+
+X, y = mnist.data, mnist.target
+print(X)
+print(X.shape)       # 28 x 28 개의 픽셀 특징을 가진 이미지 70,000개
+print(y)
+print(y.shape)
+
+import matplotlib.pyplot as plt
+
+def plot_digit(image_data):
+    image = image_data.reshape(28, 28)
+    plt.imshow(image, cmap="binary")
+    plt.axis("off")
+
+some_digit = X[0]
+plot_digit(some_digit)
+plt.show()
+
+print(y[0])         # 샘플 데이터 레이블 확인 (=5)
