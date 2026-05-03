a1dfdb5822cd0fe864a5ea2831fbd05f689ae163
박찬우 <pcw1443@gmail.com>
2026-05-03 14:06:48 +0900

    Step 3: 코드 작성

--- week07/classification.py
+++ week07/classification.py
@@ -23,3 +23,25 @@ plot_digit(some_digit)
 plt.show()
 
 print(y[0])         # 샘플 데이터 레이블 확인 (=5)
+
+# 10x10 그림 생성
+plt.figure(figsize=(9, 9))
+for idx, image_data in enumerate(X[:100]):
+    plt.subplot(10, 10, idx + 1)
+    plot_digit(image_data)
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.show()
+
+# train/test 데이터셋 나누기
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+# 이진 분류기 훈련
+y_train_5 = (y_train == '5')  # 5는 True고, 다른 숫자는 모두 False
+y_test_5 = (y_test == '5')
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+sgd_clf.predict([some_digit])
