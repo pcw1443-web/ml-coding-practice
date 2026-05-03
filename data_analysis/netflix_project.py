2b5558302a1e4a2067437b3351f1ff5824852c90
박찬우 <pcw1443@gmail.com>
2026-05-03 15:24:09 +0900

    Step 3: 코드 작성

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -25,3 +25,24 @@ for i in netflix.columns :
     if missingValueRate > 0 :
         print("{} null rate: {}%".format(i,round(missingValueRate, 2)))
 
+# .fillna( ) : 결측치를 다른 값으로 대체하여 처리
+# 결측치 비율 : country(9.44%)
+netflix['country'] = netflix['country'].fillna('No Data')
+
+# .replace(np.nan, 'b') : 결측치를 문자열 바꾸기 함수를 통해 처리
+# 결측치 비율 : director(29.91%), cast(9.37%)
+netflix['director'] = netflix['director'].replace(np.nan, 'No Data')
+netflix['cast'] = netflix['cast'].replace(np.nan, 'No Data')
+
+# .dropna(axis = 0) : 결측치가 있는 행 전체 제거
+# 원본 객체를 수정하려면 inplace = True 옵션 추가
+# 결측치 비율 : date_added(0.11%), rating(0.05%), duration(0.03%)
+netflix.dropna(axis = 0, inplace=True)
+
+# .info() : 열에 대한 요약 정보 확인
+# 8807 rows(원본 데이터 행 개수) - 17 rows(결측치 행) = 8790 rows(결측치가 제거된 행 개수)
+netflix.info()
+
+# 데이터프레임의 각 컬럼별 결측치 개수 반환
+# isna() == isnull() : 결측 값은 True 반환하고, 그 외에는 False 반환
+netflix.isna().sum()
