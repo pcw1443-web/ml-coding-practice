120bea7a8b40c450d93e3a3bf76d5801e41da437
박찬우 <pcw1443@gmail.com>
2026-05-14 17:50:02 +0900

    step 8 코드 작성

--- week08/regression.py
+++ week08/regression.py
@@ -90,3 +90,37 @@ m = 20
 X = 3 * np.random.rand(m, 1)
 y = 1 + 0.5 * X + np.random.randn(m, 1) / 1.5
 X_new = np.linspace(0, 3, 100).reshape(100, 1)
+
+# 방금 생성한 데이터셋을 간단히 살펴보기
+plt.figure(figsize=(6, 4))
+plt.plot(X, y, ".")
+plt.xlabel("$x_1$")
+plt.ylabel("$y$  ", rotation=0)
+plt.axis([0, 3, 0, 3.5])
+plt.grid()
+plt.show()
+
+from sklearn.linear_model import Ridge
+
+ridge_reg = Ridge(alpha=0.1, solver="cholesky")
+ridge_reg.fit(X, y)
+print(ridge_reg.predict([[1.5]]))
+
+sgd_reg = SGDRegressor(penalty="l2", alpha=0.1 / m, tol=None,
+                       max_iter=1000, eta0=0.01, random_state=42)
+sgd_reg.fit(X, y.ravel())  # fit()은 1D 타겟을 기대하므로 y.ravel()을 사용합니다.
+print(sgd_reg.predict([[1.5]]))
+
+# 확률적 평균 GD(solver="sag")를 사용하면
+# 이전과 거의 동일한 솔루션을 얻을 수 있음을 보여줍니다.
+ridge_reg = Ridge(alpha=0.1, solver="sag", random_state=42)
+ridge_reg.fit(X, y)
+print(ridge_reg.predict([[1.5]]))
+
+# 라쏘 회귀
+
+from sklearn.linear_model import Lasso
+
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(X, y)
+print(lasso_reg.predict([[1.5]]))
