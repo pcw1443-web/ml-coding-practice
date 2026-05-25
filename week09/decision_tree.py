ccae4bf73988a784f292e7ac181d45597dac383a
박찬우 <pcw1443@gmail.com>
2026-05-25 16:24:32 +0900

    step 2 코드 작성

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -1,2 +1,30 @@
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
+
+iris = load_iris(as_frame=True)
+X_iris = iris.data[["petal length (cm)", "petal width (cm)"]].values
+y_iris = iris.target
+
+tree_clf = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree_clf.fit(X_iris, y_iris)
+
+from sklearn.tree import export_graphviz
+
+export_graphviz(
+        tree_clf,
+        out_file="iris_tree.dot",
+        feature_names=["petal length (cm)", "petal width (cm)"],
+        class_names=iris.target_names,
+        rounded=True,
+        filled=True
+    )
+
+from graphviz import Source
+
+Source.from_file("iris_tree.dot")
+
+# 클래스 확률 추정
+
+print(tree_clf.predict_proba([[5, 1.5]]).round(3))
+print(tree_clf.predict([[5, 1.5]]))
+
