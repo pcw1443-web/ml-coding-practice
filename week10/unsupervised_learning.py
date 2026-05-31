395e7c21a69690dd38957906c75af5049cb46d3b
박찬우 <pcw1443@gmail.com>
2026-05-31 15:09:38 +0900

    step 12 코드 작성

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -112,3 +112,16 @@ print(dbscan.labels_[:10])
 
 print(dbscan.core_sample_indices_[:10])
 print(dbscan.components_)
+
+def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
+    core_mask = np.zeros_like(dbscan.labels_, dtype=bool)
+    core_mask[dbscan.core_sample_indices_] = True
+    anomalies_mask = dbscan.labels_ == -1
+    non_core_mask = ~(core_mask | anomalies_mask)
+
+    cores = dbscan.components_
+    anomalies = X[anomalies_mask]
+    non_cores = X[non_core_mask]
+
+
+    
